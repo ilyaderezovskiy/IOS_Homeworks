f1f56d49e8f574acbcf1e9c1d23a40d0c084b186
ilyaderezovskiy <71149562+ilyaderezovskiy@users.noreply.github.com>
2021-09-23 22:47:53 +0300

    Homework 2

---
+++ idderezovskiyPW2/idderezovskiyPW2/SettingsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsViewController.swift
+//  idderezovskiyPW2
+//
+//  Created by Ilya Derezovskiy on 23.09.2021.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        setupCloseButton()
+    }
+    
+    var delegate: ((UIColor) -> ())?
+
+    
+    private func setupCloseButton() {
+        let button = UIButton(type: .close)
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor,
+            constant: -10
+        ).isActive = true
+        button.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: 10
+        ).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        button.widthAnchor.constraint(
+            equalTo: button.heightAnchor
+        ).isActive = true
+        button.addTarget(self, action: #selector(closeScreen), for: .touchUpInside)
+     }
+    
+    @objc
+    private func closeScreen() {
+        dismiss(animated: true, completion: nil)
+    }
+}
